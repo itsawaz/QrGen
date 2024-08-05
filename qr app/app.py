d722f40ef33f9430d0a6fde1af8a8727f010af9a
Neeraj Singh <helloneerajkumarsingh@gmail.com>
2024-08-06 00:18:36 +0530

    Code uploaded

---
+++ qr app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, send_file, render_template
+import qrcode
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        data = request.form.get('data')
+        if not data:
+            return "Error: No data provided", 400
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_io = BytesIO()
+        img.save(img_io, 'PNG')
+        img_io.seek(0)
+
+        return send_file(img_io, mimetype='image/png')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
